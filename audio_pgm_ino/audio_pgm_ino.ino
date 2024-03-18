d7d1948485172baea0e961da9dd6d9edf597e435
Brittney Morales <brittneymrls@gmail.com>
2024-03-18 17:57:58 -0600

    able to start the choir with main button

--- audio_pgm_ino/audio_pgm_ino.ino
+++ audio_pgm_ino/audio_pgm_ino.ino
@@ -6,13 +6,17 @@
 TMRpcm tmrpcm;
 const byte nano1 = 7;
 const byte nano2 = 8;
+const byte nano3 = 6;
 
 const byte interruptPin = 2;
-// const byte interruptPin2 = 3;
 volatile byte state = LOW;
+volatile bool startFlag = false;
 
 void setup()
 {
+  startFlag = false;
+  state = LOW;
+
   tmrpcm.speakerPin=9;
 
   Serial.begin(9600);
@@ -25,28 +29,39 @@ void setup()
   tmrpcm.setVolume(6);
 
   /* interrupt setup */
-  pinMode(nano1 , OUTPUT);
-  pinMode(nano2 , OUTPUT);
+  pinMode(nano1, OUTPUT);
+  pinMode(nano2, OUTPUT);
+  pinMode(nano3, OUTPUT);
 
   pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  tmrpcm.play("chrd.wav");
-  delay(10909);
-  Serial.println("speaker 1");
+  if(startFlag){
+    tmrpcm.play("chrd.wav");
+    delay(10909);
+    // Serial.println("main 1");
 
-  tmrpcm.stopPlayback();
-  // delay(1000);
+    tmrpcm.stopPlayback();
+
+    // Serial.println(startFlag);
+    
+    state = HIGH;
+    digitalWrite(nano1, state);
+    digitalWrite(nano2, state);
+    digitalWrite(nano2, state);
+    // delay(200);
+  }
+  // delay(2000);
+  // Serial.println("i'm in the loop");
+
 }
 
 void blink() {
-  state = !state;
-  // digitalWrite(ledPin, state);
-  digitalWrite(nano1, state);
-  digitalWrite(nano2, state);
+  startFlag = true;
+    // Serial.println("startflag method");
 }
 
 
