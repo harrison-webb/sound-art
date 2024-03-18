e3e98a1f198f05c4230e4d92007e976e2f429ddc
Harrison Webb <harrison.dev.00@gmail.com>
2024-03-18 17:38:24 -0600

    Huge speaker arduino with bass.wav

---
+++ audio_pgm_ino/bass_huge_spk/bass_huge_spk.ino
@@ -0,0 +1,46 @@
+#include "SD.h"
+#define SD_ChipSelectPin 10
+#include "TMRpcm.h"
+#include "SPI.h"
+
+TMRpcm tmrpcm;
+
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void setup()
+{
+  state = LOW;
+  tmrpcm.speakerPin=9;
+
+  Serial.begin(9600);
+
+  if(!SD.begin(SD_ChipSelectPin))
+  {
+    Serial.println("SD fail");
+    return;
+  }
+  tmrpcm.setVolume(6);
+
+  pinMode(interruptPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(state)
+    {
+    tmrpcm.play("bass.wav"); //big speaker
+
+    delay(10909);
+    Serial.println("speaker 1");
+
+    tmrpcm.stopPlayback();
+    }
+}
+
+void blink() {
+  state = !state;
+}
+
+
